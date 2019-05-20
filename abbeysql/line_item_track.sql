8d758c69ac001c8d2db3254e96dd1a47e842a4a5
Abbey Brown <42549955+brownabs@users.noreply.github.com>
2019-05-20 12:10:44 -0500

    Update line_item_track.sql

---
+++ abbeysql/line_item_track.sql
@@ -0,0 +1,6 @@
+--Provide a query that includes the purchased track name with each invoice line item
+
+SELECT il.InvoiceLineId, il.InvoiceId, il.TrackId InvoiceTrackId, il.UnitPrice, il.Quantity, t.Name TrackName, t.TrackId TrackId
+FROM InvoiceLine il
+JOIN Track t ON t.TrackId = il.TrackId
+ORDER BY il.InvoiceId;
