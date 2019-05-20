8a71d8fde3cea67cb476a74f61fd27886a1d66d0
Abbey Brown <42549955+brownabs@users.noreply.github.com>
2019-05-20 13:15:45 -0500

    Update total_invoices_{2009}.sql

--- abbeysql/total_invoices_{2009}.sql
+++ abbeysql/total_invoices_{2009}.sql
@@ -4,3 +4,16 @@ SELECT COUNT(InvoiceId) Invoices
 FROM Invoice
 WHERE YEAR(InvoiceDate) = 2009
 ;
+
+--How many Invoices were there in 2009 and 2011
+
+select NumberOfInvoices, InvoiceYear
+FROM (
+    select COUNT(i.InvoiceId) NumberOfInvoices,
+            Year(i.InvoiceDate) InvoiceYear
+            from Invoice i
+            group by Year(i.InvoiceDate)
+) as Aggregate
+where Aggregate.InvoiceYear = '2011'
+or Aggregate.InvoiceYear = '2009'
+;
